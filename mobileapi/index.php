92d247bdfcc677c555985a23945ab9331b787e59
Orgdevelopers <kinddusingh1k2k3@gmail.com>
2023-01-24 21:36:19 +0530

    31

--- mobileapi/index.php
+++ mobileapi/index.php
@@ -7,10 +7,10 @@ require "Controller/AppController.php" ;
 $php_self =  $_SERVER['PHP_SELF'];
 $request_uri =  $_SERVER['REQUEST_URI'];
 
-$php_self=str_replace("/","",$php_self);
-$php_self=str_replace("index.php","/",$php_self);
+// $php_self=str_replace("/","",$php_self);
+// $php_self=str_replace("index.php","/",$php_self);
 
-$request_uri=str_replace($php_self,"",$request_uri);
+// $request_uri=str_replace($php_self,"",$request_uri);
 
 //check api key
 //verfiy_api_key();
@@ -19,11 +19,11 @@ if(strpos($request_uri,"?")){
 }
 
 $request = explode('/',$request_uri);
-echo $_SERVER['PHP_SELF']. "<br><br>";
-echo $_SERVER['REQUEST_URI']. "<br><br>";
-echo $request_uri . "<br><br>";
-echo json_encode($request);
-die;
+// echo $_SERVER['PHP_SELF']. "<br><br>";
+// echo $_SERVER['REQUEST_URI']. "<br><br>";
+// echo $request_uri . "<br><br>";
+// echo json_encode($request);
+// die;
 
 $controller = new AppController();
 $controller->handle_rquest($request);

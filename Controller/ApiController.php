cf04099cc1b7b475fe88e3a019cab337ef2915dc
Orgdevelopers <kinddusingh1k2k3@gmail.com>
2023-02-26 11:32:10 +0530

    Update ApiController.php

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -1311,6 +1311,8 @@ class ApiController extends Controller {
             $settings = $this->AppSettings->getAppSettings();
             $methods = json_decode($settings['withdraw_methods'],true);
 
+            $this->params = json_decode('{"user_id":"1","PayPal":"test@gmail.com","Crypto (USDT-TRC)":"sbdhhd","Binance (email)":"sjjdjd"}',true);
+
             foreach ($methods as $key => $method) {
                 if(isset($this->params[$method['name']])){
                     //check if already exists
